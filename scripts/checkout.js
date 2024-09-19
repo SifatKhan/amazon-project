877488928223c35446d2c25b4b4e3bb09ab4b475
SifatKhan <sifatullah0314@yahoo.com>
2024-09-20 02:06:04 +0500

    promise and fetch implemented

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,9 +2,25 @@ import { renderOrderSummary } from './checkout/orderSummary.js'
 import { renderPaymentSummary } from './checkout/paymentSummary.js';
 // import "../data/cart-class.js"
 // import '../data/backend-practice.js'
-import { loadProducts } from '../data/products.js';
+import { loadProducts, loadProductsFetch } from '../data/products.js';
 
-loadProducts(() => {
+loadProductsFetch().then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 })
+
+/* Promise */
+// new Promise((resolve) => {
+//     loadProductsFetch(() => {
+//         resolve();
+//     })
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
+
+/* callback */
+// loadProducts(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
